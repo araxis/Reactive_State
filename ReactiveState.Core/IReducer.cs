8ec0d8e3661d5e115843de0d69b525fdfd1a8078
meisamalifallahi@gmail.com <meisamalifallahi@gmail.com>
2023-11-26 16:11:30 +0100

    refactor: Simplify IReducer interface by removing ShouldReduce method
    - Remove ShouldReduce method from IReducer interface.
    - Delete the IReducer<TState> interface variant.

--- ReactiveState.Core/IReducer.cs
+++ ReactiveState.Core/IReducer.cs
@@ -2,9 +2,5 @@
 
 public interface IReducer<TState, in TAction>
 {
-    bool ShouldReduce(TState state, TAction action) => true;
     TState Reduce(TState state, TAction action);
-}
-public interface IReducer<TState> : IReducer<TState, object>
-{
 }
--- ReactiveState/IAsyncReducer.cs
+++
@@ -1,9 +0,0 @@
-﻿using ReactiveState.Core;
-
-namespace ReactiveState;
-
-internal interface IAsyncReducer<TState> : IReducer<TState>
-{
-    Type ActionType { get; }
-    Task ReduceAsync(TState state, object action,Action<TState> updateState, CancellationToken cancellationToken = default);
-}
